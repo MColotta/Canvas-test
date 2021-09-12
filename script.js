6a40a2a7835043af54478ceefb500e20f3cc36ac
Matias <>
2021-09-12 00:27:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var canvas;
+var ctx;
+var fps = 50;
+
+var personaje = function(x,y){
+    this.x = x;
+    this.y = y;
+    this.derecha = true;
+
+    this.dibuja = function(){
+        ctx.fillStyle = '#FF000';
+        ctx.fillRect(this.x, this.y, 50, 50);
+    }
+
+    this.mueve = function(velocidad){
+        if(this.derecha == true){
+            if(this.x < 400)
+                this.x+=velocidad;
+            else{
+                this.derecha = false;
+            }
+        }else{
+            if(this.x > 50)
+                this.x-=velocidad;
+            else{
+                this.derecha = true;
+        }
+    }
+    }
+}
+
+var per1 = new personaje(10,50);
+var per2 = new personaje(10,120);
+var per3 = new personaje(10,230);
+
+function inicialize(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    setInterval(function(){
+        principal();
+    },1000/fps);
+}
+
+function borraCanvas(){
+    canvas.width = 500;
+    canvas.height = 400;
+}
+
+function principal(){
+    borraCanvas();
+
+    per1.dibuja();
+    per2.dibuja();
+    per3.dibuja();
+
+    per1.mueve(1);
+    per2.mueve(3);
+    per3.mueve(7);
+}
+
+inicialize();
+principal();
